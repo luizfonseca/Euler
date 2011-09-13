3bcb07d4c957c3f8454d4366168cbe441002f280
Luiz Fonseca <runeroniek@gmail.com>
2011-09-13 15:05:50 -0400

    Test case modifiedw

--- 001.hs
+++ 001.hs
@@ -1,5 +1,3 @@
 -- Find the sum of all the multiples of 3 or 5 below 1000
-a =  [x | x <- [0..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+a = [x | x <- [0..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 result = (if sum a == 233168 then True else False)
-
-
